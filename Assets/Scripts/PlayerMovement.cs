b1f2483d0c35963781d4b286d550fdae6051b43d
Godoy6 <sergiogodoyromero@gmail.com>
2025-04-25 19:53:40 +0200

    bj

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+
+    private Rigidbody2D rb2D;
+    private Vector2 moveInput;
+
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveInput = new Vector2(moveX, moveY).normalized;
+    }
+
+    void FixedUpdate()
+    {
+        rb2D.velocity = moveInput * speed;
+    }
+}
