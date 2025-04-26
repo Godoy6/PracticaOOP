1e4b69448a3403f18c70e3457f0f9196a448ce64
Godoy6 <sergiogodoyromero@gmail.com>
2025-04-26 21:04:41 +0200

    Avances

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance; // Instancia estática para implementar el patrón Singleton
+
+    void Awake()
+    {
+        /*SINGLETON*/
+        if (!instance) // Si la instancia aún no existe
+        {
+            instance = this; // Asigna esta instancia a la variable estática
+            DontDestroyOnLoad(gameObject); // Hace que el objeto no se destruya al cargar una nueva escena
+        }
+        else
+        {
+            Destroy(gameObject); // Si ya existe una instancia, destruye este objeto
+        }
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
